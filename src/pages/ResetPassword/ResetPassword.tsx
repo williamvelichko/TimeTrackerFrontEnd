f3e3cd3a3f1850f58b5a83b81764ffea812895c1
williamvelichko <williamvelichko2003@gmail.com>
2024-04-01 15:11:54 -0700

    repo connection

--- src/pages/ResetPassword/ResetPassword.tsx
+++ src/pages/ResetPassword/ResetPassword.tsx
@@ -5,7 +5,7 @@ const ResetPassword = () => {
   return (
     <div className="container d-flex justify-content-center flex-column align-items-center h-100 w-100">
       <div className="w-100">
-        <h3 className="reset-text fw-bold text-start mb-4 ">Forgot Password</h3>
+        <h3 className="reset-text fw-bold text-start mb-4">Forgot Password</h3>
       </div>
       <ResetForm />
     </div>
